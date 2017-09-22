113a6043b8c9a5e9968cf1b537ca1b422c96cd38
unflypig <1085025884@qq.com>
2017-09-22 16:54:33 +0800

    Create log_test.c

---
+++ log_test.c
@@ -0,0 +1,13 @@
+/** test.c **/
+
+#include "stdio.h"
+#include "stdlib.h"
+#include "log.h"
+int main(int argv,char**argc){
+	printf("%s\n",argc[0]);
+	LogWrite(INFO,"%s","Hello World!");
+	LogWrite(DEBUG,"%s","H.e.l.l.o W.o.r.l.d!");
+	LogWrite(WARN,"%s","H e l l o W o r l d!");
+	LogWrite(ERROR,"%s","Hallo World!");
+	return 0;
+}
